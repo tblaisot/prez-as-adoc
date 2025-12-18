e4ce22f2d54d5388c17fbc484944b32dbeb5c1c1
tblaisot <thomas.blaisot@gmail.com>
2025-12-18 22:40:39 +0100

    feat: rework the vite plugin (from IA)

--- vite/plugins/index.js
+++ vite/plugins/index.js
@@ -1,1 +1,3 @@
-export {prezAsAdoc} from './prez-as-adoc-plugin.js'
+export {createPrezAsAdocAsciidocConverter} from './prez-as-adoc-asciidoc-converter.js';
+export {vitePluginPrezAsAdoc} from './vite-plugin-prez-as-adoc.js';
+export {vitePluginPrezAsAdoc as prezAsAdoc} from './vite-plugin-prez-as-adoc.js';
