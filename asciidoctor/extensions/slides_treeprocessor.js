149ad14aaf146dc6f6036cf21a39e658cf4c39fa
Thomas Blaisot <3236741+tblaisot@users.noreply.github.com>
2025-12-20 23:46:05 +0100

    feat: support for hidden titles and fix the first slide (deck-title)

--- asciidoctor/extensions/slides_treeprocessor.js
+++ asciidoctor/extensions/slides_treeprocessor.js
@@ -2,9 +2,13 @@ import {HELPERS} from "@tblaisot/asciidoctorjs-templates-js";
 
 const {sectionTitle, slice_text, isEmptyString} = HELPERS;
 
+const NO_TITLE = '!';
+
 export function debugAST(node) {
     return {
         context: node.getContext(),
+        role: node.getRole(),
+        attributes: node.getAttributes(),
         blocks: node.getBlocks().map(n => debugAST(n)),
     }
 }
@@ -12,6 +16,7 @@ export function debugAST(node) {
 export function register(registry) {
     registry.treeProcessor(function () {
         const self = this
+
         self.process(function (document) {
 
             // document.findBy({context: 'page_break'}).forEach((node) => {
@@ -42,16 +47,25 @@ export function register(registry) {
                             level: section.getLevel()
                         }
                     );
+
+                    const section_title = sectionTitle(section);
                     const title = self.createBlock(
                         slide,
                         'title',
-                        sectionTitle(section),
+                        section_title,
                         {
                             level: section.getLevel(),
                             slot: 'title'
                         }
                     );
-                    slide.getBlocks().push(title)
+                    if (section_title === NO_TITLE
+                        || section.isOption('notitle')
+                        || section.isOption('conceal')) {
+                        slide.addRole('hidden-title');
+                    }
+
+                    slide.getBlocks().push(title);
+
                     section.getBlocks().forEach((block) => {
                         slide.getBlocks().push(block);
                     })
@@ -67,12 +81,12 @@ export function register(registry) {
             // create title slide
             const attrName = document.getAttribute('slide-template-attr', 'slide-template', 'slide-template-attr');
 
-            const title_slide = self.createBlock(
+            const deck_title_slide = self.createBlock(
                 document,
                 'slide',
                 '',
                 {
-                    [attrName]: 'title-slide',
+                    [attrName]: 'deck-title',
                     ...document.getAttributes(),
                     level: 0
                 }
@@ -80,35 +94,41 @@ export function register(registry) {
 
             const _title_obj = document.getDoctitle({partition: true, use_fallback: true});
             if (_title_obj.subtitle) {
-                const _slice = document.getHeader().isOption('slice');
-                const title1 = self.createBlock(title_slide, 'title', slice_text(_title_obj.main, _slice), {
+                const _slice = document.getHeader().isOption('slice'); // FIXME
+                const title1 = self.createBlock(deck_title_slide, 'title', slice_text(_title_obj.main, _slice), {
                     level: 1,
                     slot: 'title'
                 });
-                title_slide.getBlocks().push(title1)
+                deck_title_slide.getBlocks().push(title1)
                 const subtitle = slice_text(_title_obj.subtitle, _slice);
                 if (!isEmptyString(subtitle)) {
-                    const title2 = self.createBlock(title_slide, 'title', subtitle, {
+                    const title2 = self.createBlock(deck_title_slide, 'title', subtitle, {
                         level: 2,
                         slot: 'subtitle'
                     });
-                    title_slide.getBlocks().push(title2)
+                    deck_title_slide.getBlocks().push(title2)
                 }
             } else {
-                const title = self.createBlock(title_slide, 'title', document.getHeader().getTitle(), {
+                const title = self.createBlock(deck_title_slide, 'title', document.getHeader().getTitle(), {
                     level: 1,
                     slot: 'title'
                 });
-                title_slide.getBlocks().push(title)
+                deck_title_slide.getBlocks().push(title)
             }
 
             const preambles = document.findBy({context: 'preamble'});
-            title_slide.getBlocks().push(...preambles);
-
-            document.getBlocks().splice(0, 0, title_slide);
-
+            if(preambles.length === 1) {
+                preambles[0].getBlocks().forEach((block) => {
+                    deck_title_slide.getBlocks().push(block);
+                });
+                const parentBlocks = preambles[0].getParent().getBlocks();
+                parentBlocks[parentBlocks.indexOf(preambles[0])] = deck_title_slide;//self.createBlock(preambles[0].getParent(), 'empty','', {}); // replace node from parent
+            } else {
+                document.getBlocks().splice(0, 0, deck_title_slide);
+            }
             // remove preamble block from document
-            document.getBlocks().splice(document.getBlocks().findIndex(b => b === preambles[0]), 1);
+            // document.getBlocks()[document.getBlocks().findIndex(b => b === preambles[0])] = self.createBlock(document, 'empty', '', {});
+
             return document;
         })
     })
